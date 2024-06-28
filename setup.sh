b7b82d1d5d5a2b84a8ccf78eeac2bcada8d09842
paul <paul.weiss@iohk.io>
2024-06-28 17:53:00 -0400

    initial

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## todo:
+## add params for different os, and cardano versions
+
+mkdir cardano
+mkdir mainnet
+mkdir mainnet/config
+cd cardano
+wget https://github.com/IntersectMBO/cardano-node/releases/download/8.9.3/cardano-node-8.9.3-linux.tar.gz
+tar -xvf cardano-node-8.9.3-linux.tar.gz
+
+echo 'export PATH=~/cardano/bin:$PATH' >> ~/.bashrc
+echo 'export CARDANO_NODE_SOCKET_PATH=~/mainnet/node.socket' >> ~/.bashrc
+
