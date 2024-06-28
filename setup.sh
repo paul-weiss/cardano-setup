a06e313af3d0f058c03877260bace4b1529a334f
paul <paul.weiss@iohk.io>
2024-06-28 18:04:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ cd cardano
 wget https://github.com/IntersectMBO/cardano-node/releases/download/8.9.3/cardano-node-8.9.3-linux.tar.gz
 tar -xvf cardano-node-8.9.3-linux.tar.gz
 
-echo 'export PATH=$(pwd)/cardano/bin:$PATH' >> ~/.bashrc
-echo 'export CARDANO_NODE_SOCKET_PATH=$(pwd)/mainnet/node.socket' >> ~/.bashrc
+echo "export PATH=$(pwd)/cardano/bin:$PATH" >> ~/.bashrc
+echo "export CARDANO_NODE_SOCKET_PATH=$(pwd)/mainnet/node.socket" >> ~/.bashrc
 
