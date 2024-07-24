971303074f11efc6ba0f7603cf775f2cc36659e9
paul <paul.weiss@iohk.io>
2024-07-24 11:58:50 -0400

    Update installers.sh

--- installers.sh
+++ installers.sh
@@ -78,7 +78,7 @@ install_cardano_db_sync() {
 install_postgresql() {
     sudo systemctl stop postgresql.service
     sudo apt remove -y postgresql
-    sudo apt install postgresql postgresql-contrib
+    sudo apt install -y postgresql postgresql-contrib
     sudo systemctl start postgresql.service
     sudo -u postgres psql -U postgres -f ~/cardano-setup/db/setup.sql
 }
