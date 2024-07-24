8ae4a15dbc441f02c85f8127e8f75e7b67857969
paul <paul.weiss@iohk.io>
2024-07-24 10:40:53 -0400

    Update setup-preview.sh

--- setup-preview.sh
+++ setup-preview.sh
@@ -18,8 +18,8 @@ export OGMIOS_VERSION="6.5.0"
 # prerequisites
 sudo apt-get install unzip
 
+install_postgresql
 install_cardano_node $CARDANO_ENV $CARDANO_VERSION
 install_cardano_db_sync $CARDANO_ENV $DB_SYNC_VERSION
-install_postgresql
 install_ogmios $CARDANO_ENV $OGMIOS_VERSION
 install_kupo $CARDANO_ENV $KUPO_VERSION $KUPO_PATCH_VERSION
