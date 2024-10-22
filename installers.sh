e92e2f740128821602544b5e223a2b90068ca63e
paul <paul.weiss@iohk.io>
2024-10-22 16:33:04 -0400

    Update installers.sh

--- installers.sh
+++ installers.sh
@@ -14,7 +14,7 @@ install_cardano_node() {
     sudo mkdir /usr/local/etc/cardano-node
     sudo mkdir /var/lib/cardano-node
     sudo mkdir /var/lib/cardano-node/db
-    sudo chmod 777 /var/lib/cardano-node
+    sudo chmod -R 777 /var/lib/cardano-node
     sudo cp -r svc/ /usr/local/etc/cardano-node
     sudo mkdir /usr/local/etc/cardano-node/$1
 
