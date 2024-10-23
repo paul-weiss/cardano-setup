4054c1d9bdad35a639502c157b8d21d297d5e47a
paul <paul.weiss@iohk.io>
2024-10-22 23:23:15 -0400

    Create start_private.sh

---
+++ svc/start_private.sh
@@ -0,0 +1,6 @@
+bin/cardano-node run \
+  --topology env/private/topology.json \
+  --database-path db \
+  --socket-path env/private/node.socket \
+  --port 3434 \
+  --config env/private/config.json
